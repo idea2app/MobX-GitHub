9be6e4473c3145683d4f277fe0bea1980747af79
TechQuery <shiy2008@gmail.com>
2025-05-28 02:09:50 +0800

    [fix] Pagination path & parameter of Organization API

--- source/Organization.ts
+++ source/Organization.ts
@@ -8,16 +8,21 @@ export type Organization = components['schemas']['organization-full'];
 
 export class OrganizationModel extends Stream<Organization>(ListModel) {
     client = githubClient;
-    baseURI = 'orgs';
+    baseURI = '';
+
+    constructor(public user = '') {
+        super();
+        this.baseURI = user ? `users/${user}/orgs` : 'user/orgs';
+    }
 
     async *openStream() {
         var per_page = this.pageSize,
             since: number | undefined,
             count = 0;
 
-        while (true) {
+        for (let page = 0; ; page++) {
             const { body } = await this.client.get<Organization[]>(
-                `user/${this.baseURI}?${buildURLData({ per_page, since })}`
+                `${this.baseURI}?${buildURLData({ per_page, page, since })}`
             );
             if (!body![0]) break;
 
