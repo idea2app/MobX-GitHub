ce7aa7bbb4902221ee868a8eee1d63bcf510ed28
TechQuery <shiy2008@gmail.com>
2025-05-28 02:30:29 +0800

    [fix] List base URI of Organization API

--- source/Organization.ts
+++ source/Organization.ts
@@ -8,21 +8,21 @@ export type Organization = components['schemas']['organization-full'];
 
 export class OrganizationModel extends Stream<Organization>(ListModel) {
     client = githubClient;
-    baseURI = '';
+    baseURI = 'orgs';
 
     constructor(public user = '') {
         super();
-        this.baseURI = user ? `users/${user}/orgs` : 'user/orgs';
     }
 
     async *openStream() {
         var per_page = this.pageSize,
             since: number | undefined,
             count = 0;
+        const baseURI = this.user ? `users/${this.user}/` : '';
 
         for (let page = 0; ; page++) {
             const { body } = await this.client.get<Organization[]>(
-                `${this.baseURI}?${buildURLData({ per_page, page, since })}`
+                `${baseURI}orgs?${buildURLData({ per_page, page, since })}`
             );
             if (!body![0]) break;
 
