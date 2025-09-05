ab99f5be082c1bcc8793d0a5c4a8e4437e395dbf
South Drifter <shiy2008@gmail.com>
2025-09-06 07:18:53 +0800

    [add] Issue, Pull Request, Discussion & Contributor models (#4)

--- source/Repository.ts
+++ source/Repository.ts
@@ -1,30 +1,21 @@
 import { components } from '@octokit/openapi-types';
-import { encodeBase64 } from 'koajax';
 import { memoize } from 'lodash';
 import { Filter, ListModel, toggle } from 'mobx-restful';
-import {
-    PickSingle,
-    averageOf,
-    buildURLData,
-    groupBy,
-    makeArray
-} from 'web-utility';
+import { averageOf, buildURLData, groupBy } from 'web-utility';
 
+import { githubClient } from './client';
+import { Contributor, ContributorModel } from './Contributor';
+import { Issue, IssueModel } from './Issue';
 import { OrganizationModel } from './Organization';
 import { User } from './User';
-import { githubClient } from './client';
 
 type Repository = components['schemas']['minimal-repository'];
-export type Contributor = components['schemas']['contributor'];
-export type Issue = components['schemas']['issue'];
 
 export interface GitRepository extends Repository {
     contributors?: Contributor[];
     languages?: string[];
     issues?: Issue[];
 }
-export type GitFile = components['schemas']['content-file'];
-export type GitContent = PickSingle<components['schemas']['content-directory']>;
 
 export interface RepositoryFilter extends Filter<GitRepository> {
     relation: (keyof RepositoryModel['relation'])[];
@@ -38,10 +29,7 @@ type ReturnMap<T> = {
           : never;
 };
 
-export class RepositoryModel extends ListModel<
-    GitRepository,
-    RepositoryFilter
-> {
+export class RepositoryModel extends ListModel<GitRepository, RepositoryFilter> {
     client = githubClient;
     baseURI = '';
     indexKey = 'full_name' as const;
@@ -55,24 +43,21 @@ export class RepositoryModel extends ListModel<
 
     relation = {
         contributors: memoize(async (URI: string) => {
-            const { body } = await this.client.get<Contributor[]>(
-                `repos/${URI}/contributors?per_page=100`
-            );
-            return (
-                body?.sort((a, b) => b.contributions - a.contributions) || []
-            );
+            const [owner, repository] = URI.split('/');
+
+            const body = await new ContributorModel(owner, repository).getAll();
+
+            return body.sort((a, b) => b.contributions - a.contributions) || [];
         }),
-        issues: memoize(async (URI: string) => {
-            const { body: issuesList } = await this.client.get<Issue[]>(
-                `repos/${URI}/issues?per_page=100`
-            );
-            return issuesList!.filter(({ pull_request }) => !pull_request);
+        issues: memoize((URI: string) => {
+            const [owner, repository] = URI.split('/');
+
+            return new IssueModel(owner, repository).getAll();
         }),
         languages: memoize(async (URI: string) => {
-            const { body: languageCount } = await this.client.get<
-                Record<string, number>
-            >(`repos/${URI}/languages`);
-
+            const { body: languageCount } = await this.client.get<Record<string, number>>(
+                `repos/${URI}/languages`
+            );
             const languageAverage = averageOf(...Object.values(languageCount!));
 
             const languageList = Object.entries(languageCount!)
@@ -83,19 +68,14 @@ export class RepositoryModel extends ListModel<
         })
     };
 
-    async getOneRelation(
-        URI: string,
-        relation: RepositoryFilter['relation'] = []
-    ) {
+    async getOneRelation(URI: string, relation: RepositoryFilter['relation'] = []) {
         const relationData = await Promise.all(
             relation.map(async key => {
                 const value = await this.relation[key](URI);
                 return [key, value];
             })
         );
-        return Object.fromEntries(relationData) as ReturnMap<
-            RepositoryModel['relation']
-        >;
+        return Object.fromEntries(relationData) as ReturnMap<RepositoryModel['relation']>;
     }
 
     @toggle('downloading')
@@ -108,11 +88,7 @@ export class RepositoryModel extends ListModel<
         });
     }
 
-    async loadPage(
-        page: number,
-        per_page: number,
-        { relation }: RepositoryFilter
-    ) {
+    async loadPage(page: number, per_page: number, { relation }: RepositoryFilter) {
         const [kind, namespace] = this.baseURI.split('/'),
             isUser = kind === 'user';
 
@@ -134,45 +110,13 @@ export class RepositoryModel extends ListModel<
 
         if (!this.totalCount)
             if (!isUser)
-                ({ public_repos: totalCount } =
-                    await this.organizationStore.getOne(namespace));
+                ({ public_repos: totalCount } = await this.organizationStore.getOne(namespace));
             else {
                 const { body } = await this.client.get<User>('user');
 
-                totalCount =
-                    body!.public_repos + (body!.total_private_repos || 0);
+                totalCount = body!.public_repos + (body!.total_private_repos || 0);
             }
         return { pageData, totalCount };
-    }
-
-    @toggle('downloading')
-    async getContents(repository = this.currentOne.name, path = '') {
-        const { body } = await this.client.get<GitContent[]>(
-            `repos/${this.owner}/${repository}/contents/${path}`
-        );
-        return makeArray(body);
-    }
-
-    @toggle('uploading')
-    async updateContent(
-        path: string,
-        content: string | Blob,
-        message = `[update] ${path}`,
-        repository = this.currentOne.name
-    ) {
-        try {
-            var [{ sha }] = await this.getContents(repository, path);
-        } catch {}
-
-        const { body } = await this.client.put<{
-            content: GitContent;
-            commit: components['schemas']['commit'];
-        }>(`repos/${this.owner}/${repository}/contents/${path}`, {
-            sha,
-            message,
-            content: await encodeBase64(content)
-        });
-        return body!.content;
     }
 
     async getAllContributors() {
@@ -188,10 +132,7 @@ export class RepositoryModel extends ListModel<
         return Object.entries(userGroup)
             .map(([login, list]) => ({
                 ...list[0],
-                contributions: list.reduce(
-                    (sum, { contributions }) => sum + contributions,
-                    0
-                )
+                contributions: list.reduce((sum, { contributions }) => sum + contributions, 0)
             }))
             .sort((a, b) => b.contributions - a.contributions);
     }
